2cd17b87233b36d750b5d2af22745f529a6af13c
Transhumanist <ghostoftranshumanist@gmail.com>
2025-11-04 19:18:21 +0300

    frontend test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import { init, requestFullscreen } from '@telegram-apps/sdk-vue'
+init();
+requestFullscreen();
 createApp(App).mount('#app')
