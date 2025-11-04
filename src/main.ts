27e5a49213a1d45d2f08b5e915326e9baced9f44
Transhumanist <ghostoftranshumanist@gmail.com>
2025-11-04 19:29:05 +0300

    t

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { init, miniApp, viewport } from '@telegram-apps/sdk-vue'
 init();
+createApp(App).mount('#app')
 if (miniApp.mountSync.isAvailable()) {
   miniApp.mountSync();
   miniApp.isMounted();
@@ -10,4 +11,3 @@ viewport.mount();
 viewport.isMounted();
 await viewport.requestFullscreen();
 viewport.isFullscreen();
-createApp(App).mount('#app')
