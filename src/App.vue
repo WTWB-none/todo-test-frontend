38b3f61cec923415e7e4e60b9d31ff4220b413f3
Transhumanist <ghostoftranshumanist@gmail.com>
2025-11-04 19:14:03 +0300

    frontend test

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@ import TaskList from './components/TaskList.vue';
 import AddTaskButton from './components/AddTaskButton.vue';
 import Header from './components/Header.vue';
 import { onMounted } from 'vue';
-import { init, requestFullscreen } from '@telegram-apps/sdk';
+import { init, viewport, requestFullscreen } from '@telegram-apps/sdk';
 
 onMounted(() => {
   init();
-  requestFullscreen();
+  viewport.mount()
+  viewport.requestFullscreen();
 })
 </script>
 <template>
