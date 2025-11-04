d7938b4ad25fb516a0ebd8d33547deec198d54d3
Transhumanist <ghostoftranshumanist@gmail.com>
2025-11-04 19:47:58 +0300

    t

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@
   color: white;
   background: linear-gradient(45deg, #a020f0 25%, #0077b6);
   padding: 0;
-  padding-top: 2em;
+  padding-top: 3em;
   margin: 0;
   min-width: 100%;
   margin-bottom: 1em;
