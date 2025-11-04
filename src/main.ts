9a698a9e594546ee688ce84ca8af5571639ed170
Transhumanist <ghostoftranshumanist@gmail.com>
2025-11-04 19:26:33 +0300

    t

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { init, miniApp, requestFullscreen } from '@telegram-apps/sdk-vue'
+import { init, miniApp, viewport } from '@telegram-apps/sdk-vue'
 init();
 if (miniApp.mountSync.isAvailable()) {
   miniApp.mountSync();
   miniApp.isMounted();
 }
-requestFullscreen();
+
+if (viewport.mount.isAvailable()) {
+  viewport.mount();
+  viewport.isMounted();
+}
+
+if (viewport.requestFullscreen.isAvailable()) {
+  await viewport.requestFullscreen();
+  viewport.isFullscreen();
+}
 createApp(App).mount('#app')
